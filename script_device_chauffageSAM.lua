253cd5423cb8f25c9d62e3d34f43dc6f3caf161d
gonzo47 <gonzalez.benjamin47@gmail.com>
2019-01-25 07:16:08 +0100

    rangement du code pour les chauffages

--- script_device_chauffageSAM.lua
+++ script_device_chauffageSAM.lua
@@ -15,15 +15,15 @@ Usage:
 ------          Configuration          ------
 local   saison = 'Saison'
 local   chauffage={}
-            chauffage['sonde']          = 'Cuisine'              --Nom de la sonde de température
-            chauffage['saison']         = 'Saison'				--Nom de l'interrupteur virtuel du thermostat
-            chauffage['thermostat']     = 'Thermostat SAM'   --Interrupteur général du radiateur
-            chauffage['radiateur']      = 'Radiateur SAM'    --Nom du radiateur à allumer/éteindre
-            chauffage['fenetre']        = 'Fenetre SAM'		--Nom du detecteur de la fenetre
-            chauffage['heure_creuse']   = 'heureCreuse'			--Interrupteur gérant les heures creuses
+            chauffage['sonde']          = 'Cuisine'              	--Nom de la sonde de température
+            chauffage['saison']         = 'Saison'					--Nom de l'interrupteur virtuel du thermostat
+            chauffage['thermostat']     = 'Thermostat SAM'   		--Interrupteur général du radiateur
+            chauffage['radiateur']      = 'Radiateur SAM'    		--Nom du radiateur à allumer/éteindre
+            chauffage['fenetre']        = 'Fenetre SAM'				--Nom du detecteur de la fenetre
+            chauffage['heure_creuse']   = 'heureCreuse'				--Interrupteur gérant les heures creuses
 
-            chauffage['consigne']       = 'Consigne SAM'     --Température de consigne
-            chauffage['hysteresis']     = 0.5                   --Valeur seuil pour éviter que le relai ne cesse de commuter dans les 2 sens 
+            chauffage['consigne']       = 'Consigne SAM'     		--Température de consigne
+            chauffage['hysteresis']     = 0.5                   	--Valeur seuil pour éviter que le relai ne cesse de commuter dans les 2 sens
             chauffage['confort']        = 1
             chauffage['boost']          = 2 
 ----------------------------------------------
--- script_device_chauffageSDB.lua
+++ script_device_chauffageSDB.lua
@@ -16,14 +16,14 @@ Usage:
 local   saison = 'Saison'
 local fenetre = 'Fenetre SDB'
 local   chauffage={}
-            chauffage['sonde']          = 'SDB'              --Nom de la sonde de température
-            chauffage['saison']         = 'Saison'				--Nom de l'interrupteur virtuel du thermostat
-            chauffage['thermostat']     = 'Thermostat SDB'   --Interrupteur général du radiateur
-            chauffage['radiateur']      = 'Radiateur SDB'    --Nom du radiateur à allumer/éteindre
-            chauffage['fenetre']        = 'Fenetre SDB'		--Nom du detecteur de la fenetre
+            chauffage['sonde']          = 'SDB'        		    --Nom de la sonde de température
+			chauffage['saison']         = 'Saison'				--Nom de l'interrupteur virtuel du thermostat
+			chauffage['thermostat']     = 'Thermostat SDB'		--Interrupteur général du radiateur
+            chauffage['radiateur']      = 'Radiateur SDB'    	--Nom du radiateur à allumer/éteindre
+            chauffage['fenetre']        = 'Fenetre SDB'			--Nom du detecteur de la fenetre
             chauffage['heure_creuse']   = 'heureCreuse'			--Interrupteur gérant les heures creuses
 
-            chauffage['consigne']       = 'Consigne SDB'     --Température de consigne
+            chauffage['consigne']       = 'Consigne SDB'     	--Température de consigne
             chauffage['hysteresis']     = 0.5                   --Valeur seuil pour éviter que le relai ne cesse de commuter dans les 2 sens 
             chauffage['confort']        = 1
             chauffage['boost']          = 2 
